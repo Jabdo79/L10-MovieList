f1b0842b405aa5da135495d64203aa624cbe9c44
Jonathan Abdo <jabdo79@gmail.com>
2016-06-14 15:18:44 -0400

    finish lab, still need MoviesIO from GCGitHub

--- src/Movie.java
+++ src/Movie.java
@@ -1,5 +1,43 @@
 
 public class Movie {
 	private String title = "", category = "";
+	private int categoryNum = 0;
+	
+	public Movie(String title, String category){
+		this.title = title;
+		this.category = category;
+		
+		switch(category){
+		case "animated":
+			categoryNum = 1;
+			break;
+		case "drama":
+			categoryNum = 2;
+			break;
+		case "horror":
+			categoryNum = 3;
+			break;
+		case "scifi":
+			categoryNum = 4;
+			break;
+		}
+		
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public String getCategory(){
+		return category;
+	}
+	
+	public int getCategoryNum(){
+		return categoryNum;
+	}
+	
+	public static String getCategoryMenu(){
+		return "\nCategories\n1. Animated\n2. Drama\n3. Horror\n4. Sci-Fi\n";
+	}
 	
 }
--- src/MovieList.java
+++ src/MovieList.java
@@ -1,15 +1,55 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MovieList {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		Scanner sc = new Scanner(System.in);
+		ArrayList<Movie> movies = new ArrayList<Movie>();
+		int userChoice = 0;
+		
+		//Filling ArrayList
+		Movie userMovie;
+		movies.add(userMovie = new Movie("Titanic","drama"));
+		movies.add(userMovie = new Movie("Aliens","horror"));
+		movies.add(userMovie = new Movie("Up","animated"));
+		movies.add(userMovie = new Movie("Frozen","animated"));
+		movies.add(userMovie = new Movie("Gamer","scifi"));
+		movies.add(userMovie = new Movie("Tron","scifi"));
+		movies.add(userMovie = new Movie("The Notebook","drama"));
+		movies.add(userMovie = new Movie("The Time Traveler's Wife","drama"));
+		movies.add(userMovie = new Movie("American Beauty","drama"));
+		movies.add(userMovie = new Movie("Friday the 13th","horror"));
 		
 		System.out.println("Welcome to the Movie List Application!");
+		System.out.println("\nThere are "+movies.size()+" in this list.");
 		
 		boolean cont = true;
 		while(cont){
+			String menu = Movie.getCategoryMenu();
+			userChoice = InputCheck.getInt(sc, menu+"What category are you interested in? ", 1, 4);
+			switch(userChoice){
+			case 1:
+				System.out.println("\nAnimated\n");
+				break;
+			case 2:
+				System.out.println("\nDrama\n");
+				break;
+			case 3:
+				System.out.println("\nHorror\n");
+				break;
+			case 4:
+				System.out.println("\nSci-fi\n");
+				break;
+			}
 			
+			
+			for(int i=0; i<movies.size(); i++){
+				if(movies.get(i).getCategoryNum() == userChoice)
+					System.out.println(movies.get(i).getTitle());
+			}
+			
+			cont = InputCheck.cont(sc, "\nContinue? (y/n): ");
 		}
 		
 		sc.close();
