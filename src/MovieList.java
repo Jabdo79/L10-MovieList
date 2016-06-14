14e3dc34c7d6ca33340200b9c0b6c6c336bed76e
Jonathan Abdo <jabdo79@gmail.com>
2016-06-14 13:59:50 -0400

    first commit

---
+++ src/MovieList.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MovieList {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Movie List Application!");
+		
+		boolean cont = true;
+		while(cont){
+			
+		}
+		
+		sc.close();
+	}
+
+}
